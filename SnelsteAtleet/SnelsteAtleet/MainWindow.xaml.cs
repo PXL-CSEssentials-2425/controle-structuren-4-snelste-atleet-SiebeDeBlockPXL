18c7c23f0843e02f2317d6bc24ed8a5276b8d803
SiebeDeBlockPXL <siebe.deblock@student.pxl.be>
2024-10-04 12:29:47 +0200

    Snelste atleet oefening afgemaakt

---
+++ SnelsteAtleet/SnelsteAtleet/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnelsteAtleet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string nameFastest;
+        int timeFastest = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+            resultTextBox.Clear();
+
+            nameTextBox.Focus();
+        }
+        private void addNewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //int timeCurrent;
+            bool isValidNumber = int.TryParse(timeTextBox.Text, out int timeCurrent);
+
+            //if (isValidNumber == true)
+            if (isValidNumber) 
+            {
+                if(timeFastest == 0 || timeCurrent < timeFastest)
+                {
+                    timeFastest = timeCurrent;
+                    nameFastest = nameTextBox.Text;
+                }
+            }
+        }
+        private void fastestAthleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Text = $"De snelste atleet is {nameFastest} met een tijd van {timeFastest} seconden";
+        }
+
+
+    }
+}
